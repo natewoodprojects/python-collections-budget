4e13430eb054623e9b36a387b120b5ad7d18e669
Nate Wood <99522059+natewoodprojects@users.noreply.github.com>
2022-07-05 11:54:44 -0500

    Part 2 maybe complete

--- budget/FrequentExpenses.py
+++ budget/FrequentExpenses.py
@@ -1,4 +1,6 @@
-from .. import Expense
+from Expense import Expense
+
+import matplotlib.pyplot as plt
 
 import collections
 
@@ -13,3 +15,14 @@ for expense in expenses.list:
 
 spending_counter = collections.Counter(spending_categories)
 
+top5 = spending_counter.most_common(5)
+
+categories, count = zip(top5)
+
+fig, ax = plt.subplots()
+
+ax.bar(categories, count)
+
+ax.set_title('# of Purchases by Category')
+
+plt.shor()
